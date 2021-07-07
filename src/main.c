9e7a2af6658f4795330dbbd79728c3f31574cbcc
Joel Lehtonen <joel.lehtonen@iki.fi>
2021-07-07 15:17:33 +0300

    Reindent using Linux coding style

--- src/main.c
+++ src/main.c
@@ -16,55 +16,55 @@
 
 int main( int argc, char *argv[])
 {
-    int n_errors = 0;
-    int cumulative_errors = 0;
-    int read_counter = 0;
-    int ret;
-    modbus_t *ctx;
-    uint16_t dest [NB_REGS];
+	int n_errors = 0;
+	int cumulative_errors = 0;
+	int read_counter = 0;
+	int ret;
+	modbus_t *ctx;
+	uint16_t dest [NB_REGS];
 
-    ctx = modbus_new_rtu(UART_PATH, BAUDRATE, 'N', 8, 1);
-    modbus_rtu_set_serial_mode(ctx, MODBUS_RTU_RS485);
-    modbus_rtu_set_rts(ctx, MODBUS_RTU_RTS_DOWN);
+	ctx = modbus_new_rtu(UART_PATH, BAUDRATE, 'N', 8, 1);
+	modbus_rtu_set_serial_mode(ctx, MODBUS_RTU_RS485);
+	modbus_rtu_set_rts(ctx, MODBUS_RTU_RTS_DOWN);
 
-    if (ctx == NULL) {
-        perror("Unable to create the libmodbus context\n");
-        return -1;
-    }
+	if (ctx == NULL) {
+		perror("Unable to create the libmodbus context\n");
+		return -1;
+	}
 
-    ret = modbus_set_slave(ctx, 1);
-    if(ret < 0){
-        perror("modbus_set_slave error\n");
-        return -1;
-    }
-    ret = modbus_connect(ctx);
-    if(ret < 0){
-        perror("modbus_connect error\n");
-        return -1;
-    }
+	ret = modbus_set_slave(ctx, 1);
+	if(ret < 0){
+		perror("modbus_set_slave error\n");
+		return -1;
+	}
+	ret = modbus_connect(ctx);
+	if(ret < 0){
+		perror("modbus_connect error\n");
+		return -1;
+	}
 
-    for(;;){
-        ret = modbus_read_input_registers(ctx, 0x3104, NB_REGS, dest);
-        if(ret < 0){
-            n_errors++;
-            cumulative_errors++;
-            if(n_errors >= 3){
-                perror("modbus_read_regs error\n");
-                return -1;
-            }
-        }else{
-               n_errors = 0;
-           }
-        read_counter++;
-        printf("Read counter = %d\n",read_counter);
-        printf("Error counter = %d\n",n_errors);
-        printf("Cumulative Error counter = %d\n",cumulative_errors);
-        printf("Battery Voltage = %d\n",dest[0]);
-        printf("Battery Charging current = %d\n",dest[1]);
-//        sleep(1);
-    }
+	for(;;){
+		ret = modbus_read_input_registers(ctx, 0x3104, NB_REGS, dest);
+		if(ret < 0){
+			n_errors++;
+			cumulative_errors++;
+			if(n_errors >= 3){
+				perror("modbus_read_regs error\n");
+				return -1;
+			}
+		}else{
+			n_errors = 0;
+		}
+		read_counter++;
+		printf("Read counter = %d\n",read_counter);
+		printf("Error counter = %d\n",n_errors);
+		printf("Cumulative Error counter = %d\n",cumulative_errors);
+		printf("Battery Voltage = %d\n",dest[0]);
+		printf("Battery Charging current = %d\n",dest[1]);
+		// sleep(1);
+	}
 
-    modbus_close(ctx);
-    modbus_free(ctx);
+	modbus_close(ctx);
+	modbus_free(ctx);
 
 }
